6a8f8802448b5f5381dcfce9d6af4b320a0de0f3
Alexander Wilkinson <alexwilkinson@gmail.com>
2019-11-13 23:11:36 +0800

    added in the reading of classes and added to components along with test styling

--- main.js
+++ main.js
@@ -1,14 +1,40 @@
+const REGEX_SELECTORS = {
+  COMPLETE_COMPONENT_SELECTOR: new RegExp(/.*:.*/gm),
+  CLASS_SELECTOR: new RegExp(/.*\sclass\[(.*)]:/),
+  TAG_SELECTOR: new RegExp(/^(.*?)/gm),
+};
+
 const testHTML = `
-  h1: This is the header
+  h1 class[standard my-main-header]: This is the header
   h2: Smaller header
   p: This is a paragraph : with some stuff
   h3: Smallerer header
 `;
 
 const componentMap = {
-  h1: "<h1>{{value}}</h1>",
-  h2: "<h2>{{value}}</h2>",
-  h3: "<h3>{{value}}</h3>"
+  h1: '<h1>{{value}}</h1>',
+  h2: '<h2>{{value}}</h2>',
+  h3: '<h3>{{value}}</h3>',
+};
+
+const addClasses = (selectedComponent, initComponentString) => {
+  const classList = initComponentString.match(REGEX_SELECTORS.CLASS_SELECTOR);
+  if (!classList || classList.length < 2) {
+    return selectedComponent;
+  }
+  const classesArr = classList[1].split(' ');
+  let finalClassString = `class=' `;
+  classesArr.forEach(cls => (finalClassString = `${finalClassString} ${cls} `));
+  finalClassString = finalClassString + `'`;
+  const selectedComponentStart = selectedComponent.substring(
+    0,
+    selectedComponent.indexOf('>')
+  );
+  const selectedComponentEnd = selectedComponent.substring(
+    selectedComponent.indexOf('>')
+  );
+
+  return `${selectedComponentStart} ${finalClassString} ${selectedComponentEnd}`;
 };
 
 const getTags = (htmlString = testHTML, components = componentMap) => {
@@ -16,19 +42,23 @@ const getTags = (htmlString = testHTML, components = componentMap) => {
   const componentMatcher = new RegExp(/.*:.*/gm);
   const componentList = htmlString.match(componentMatcher);
   const componentListTypes = htmlString.match(componentTypeMatcher);
-  let output = "";
+  let output = '';
   componentList.forEach(tag => {
     const splitByTag = new RegExp(/^(.*?):/);
+    const onlyTag = new RegExp(/([A-Za-z0-9]){1,}/);
     const componentSplit = tag.trim().split(splitByTag);
-    const selectedComponent = componentMap[componentSplit[1]];
+    const foundTag = componentSplit[1].match(onlyTag)[0];
+    let selectedComponent = componentMap[foundTag];
     if (!selectedComponent) {
       return;
     }
+    selectedComponent = addClasses(selectedComponent, tag);
     const finalComponent = selectedComponent.replace(
-      "{{value}}",
+      '{{value}}',
       componentSplit[2]
     );
-    output = output + "\n" + finalComponent;
+
+    output = output + '\n' + finalComponent;
     return;
   });
 
@@ -36,12 +66,12 @@ const getTags = (htmlString = testHTML, components = componentMap) => {
 };
 
 const buildNoAppError = () => {
-  const domBody = document.querySelector("body");
+  const domBody = document.querySelector('body');
   domBody.innerHTML = `<h1>No App tag found in dom</h1>`;
 };
 
 const buildApp = (htmlString = testHTML, components = componentMap) => {
-  const app = document.querySelector(".app");
+  const app = document.querySelector('.app');
 
   if (!app) {
     return buildNoAppError();
